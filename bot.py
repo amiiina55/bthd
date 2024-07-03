59b8d35198db15a2a048b0b5f6c72e36fffccd6f
amiiina55 <Sembaeva05@list.ru>
2024-07-03 15:50:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -164,7 +164,7 @@ def main():
             )
         
         # Задача для отправки уведомления в день ДР в 9 утра по алматинскому времени
-        notify_birthday = datetime.combine(birthday, time(15, 59, 0))
+        notify_birthday = datetime.combine(birthday, time(15, 58, 0))
         notify_birthday = tz.localize(notify_birthday)
         
         if notify_birthday >= now:
